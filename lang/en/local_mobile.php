891973b271dce087e5b60958c2571821c1f428a1
Juan Leyva <juanleyvadelgado@gmail.com>
2014-05-28 13:01:14 +0200

    First commit including support for SSO

---
+++ lang/en/local_mobile.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_mobile', language 'en'.
+ *
+ * @package   local_mobile
+ * @copyright 2014 Juan Leyva <juan@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['local_mobiledescription'] = 'Plugin that extends the Mobile service features';
+$string['pluginname'] = 'Moodle Mobile extended features';
+$string['pluginnotenabledorconfigured'] = 'The Moodle Mobile local plugin must be enabled and configured in order to launch the app';
+$string['local_mobilesettings'] = 'Settings';
+$string['local_mobiletypeoflogin'] = 'Choose the type of login';
+$string['local_mobiletypeoflogin_key'] = 'Type of login';
+$string['loginintheapp'] = 'Force log in using the app';
+$string['logininthebrowser'] = 'Force log in using the default mobile browser (for SSO plugins)';
