057f140c16fae3700ef0c53245603a4c89c7afc8
Juan Leyva <juanleyvadelgado@gmail.com>
2014-06-05 22:15:53 +0200

    Improved error messages

--- lang/en/local_mobile.php
+++ lang/en/local_mobile.php
@@ -22,6 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['cannotcreatetoken'] = 'Automatically token generation is not available to site administrators (they must create a token manually in the site)';
 $string['local_mobiledescription'] = 'Plugin that extends the Mobile service features';
 $string['pluginname'] = 'Moodle Mobile extended features';
 $string['pluginnotenabledorconfigured'] = 'The Moodle Mobile local plugin must be enabled and configured in order to launch the app';
