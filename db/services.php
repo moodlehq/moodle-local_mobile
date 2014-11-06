bf14d5b7663ce291150ba671e636870d58ffb1a6
Juan Leyva <juanleyvadelgado@gmail.com>
2014-11-06 12:45:42 +0100

    MOBILE-725 New external function gradereport_user_get_grades_table

--- db/services.php
+++ db/services.php
@@ -79,7 +79,14 @@ $functions = array(
         'type'          => 'read',
         'capabilities'  => '',
     ),
-
+    'local_mobile_gradereport_user_get_grades_table' => array(
+        'classname'   => 'local_mobile_external',
+        'methodname'  => 'gradereport_user_get_grades_table',
+        'classpath'   => 'local/mobile/externallib.php',
+        'description' => 'Get the user/s report grades table for a course',
+        'type'        => 'read',
+        'capabilities'=> '',
+    )
 );
 
 $services = array(
@@ -109,7 +116,8 @@ $services = array(
             'core_message_block_contacts',
             'core_message_unblock_contacts',
             'core_message_get_contacts',
-            'core_message_search_contacts'
+            'core_message_search_contacts',
+            'local_mobile_gradereport_user_get_grades_table'
         ),
         'enabled' => 0,
         'restrictedusers' => 0,
--- externallib.php
+++ externallib.php
@@ -1378,4 +1378,223 @@ class local_mobile_external extends external_api {
         );
     }
 
+    /**
+     * Describes the parameters for get_grades_table.
+     *
+     * @return external_external_function_parameters
+     * @since Moodle 2.8
+     */
+    public static function gradereport_user_get_grades_table_parameters() {
+        return new external_function_parameters (
+            array(
+                'courseid' => new external_value(PARAM_INT, 'Course Id', VALUE_REQUIRED),
+                'userid'   => new external_value(PARAM_INT, 'Return grades only for this user (optional)', VALUE_DEFAULT, 0)
+            )
+        );
+    }
+
+    /**
+     * Returns a list of grades tables for users in a course.
+     *
+     * @param int $courseid Course Id
+     * @param int $userid   Only this user (optional)
+     *
+     * @return array the grades tables
+     * @since Moodle 2.8
+     */
+    public static function gradereport_user_get_grades_table($courseid, $userid = 0) {
+        global $CFG, $USER;
+
+        require_once($CFG->dirroot . '/group/lib.php');
+        require_once($CFG->libdir  . '/gradelib.php');
+        require_once($CFG->dirroot . '/grade/lib.php');
+        require_once($CFG->dirroot . '/grade/report/user/lib.php');
+
+        $warnings = array();
+
+        // Validate the parameter.
+        $params = self::validate_parameters(self::gradereport_user_get_grades_table_parameters(),
+            array(
+                'courseid' => $courseid,
+                'userid' => $userid)
+            );
+
+        // Compact/extract functions are not recommended.
+        $courseid = $params['courseid'];
+        $userid   = $params['userid'];
+
+        // Function get_course internally throws an exception if the course doesn't exist.
+        $course = get_course($courseid);
+
+        $context = context_course::instance($courseid);
+        self::validate_context($context);
+
+        $user = null;
+        if (!empty($userid)) {
+            $user = core_user::get_user($userid, '*', MUST_EXIST);
+        }
+
+        // Specific capabilities.
+        require_capability('gradereport/user:view', $context);
+
+        $access = false;
+
+        if (empty($user)) {
+            require_capability('moodle/grade:viewall', $context);
+            $access = true;
+        } else if ($userid == $USER->id and has_capability('moodle/grade:view', $context) and $course->showgrades) {
+            // View own grades.
+            $access = true;
+        } else if (has_capability('moodle/grade:viewall', context_user::instance($userid)) and $course->showgrades) {
+            // Can view grades of this user, parent most probably.
+            $access = true;
+        }
+
+        if (!$access) {
+            throw new moodle_exception('nopermissiontoviewgrades', 'error',  $CFG->wwwroot.  '/course/view.php?id=' . $courseid);
+        }
+
+        $gpr = new grade_plugin_return(
+            array(
+                'type' => 'report',
+                'plugin' => 'user',
+                'courseid' => $courseid,
+                'userid' => $userid)
+            );
+
+        $tables = array();
+
+        // Just one user.
+        if ($user) {
+            $report = new grade_report_user($courseid, $gpr, $context, $userid);
+            $report->fill_table();
+
+            // Notice that we use array_filter for deleting empty elements in the array.
+            // Those elements are items or category not visible by the user.
+            $tables[] = array(
+                'courseid'      => $courseid,
+                'userid'        => $user->id,
+                'userfullname'  => fullname($user),
+                'maxdepth'      => $report->maxdepth,
+                'tabledata'     => $report->tabledata
+            );
+
+        } else {
+            $defaultgradeshowactiveenrol = !empty($CFG->grade_report_showonlyactiveenrol);
+            $showonlyactiveenrol = get_user_preferences('grade_report_showonlyactiveenrol', $defaultgradeshowactiveenrol);
+            $showonlyactiveenrol = $showonlyactiveenrol || !has_capability('moodle/course:viewsuspendedusers', $context);
+
+            $gui = new graded_users_iterator($course);
+            $gui->require_active_enrolment($showonlyactiveenrol);
+            $gui->init();
+
+            while ($userdata = $gui->next_user()) {
+                $currentuser = $userdata->user;
+                $report = new grade_report_user($courseid, $gpr, $context, $currentuser->id);
+                $report->fill_table();
+
+                // Notice that we use array_filter for deleting empty elements in the array.
+                // Those elements are items or category not visible by the user.
+                $tables[] = array(
+                    'courseid'      => $courseid,
+                    'userid'        => $currentuser->id,
+                    'userfullname'  => fullname($currentuser),
+                    'maxdepth'      => $report->maxdepth,
+                    'tabledata'     => $report->tabledata
+                );
+            }
+            $gui->close();
+        }
+
+        $result = array();
+        $result['tables'] = $tables;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Creates a table column structure
+     *
+     * @return array
+     * @since  Moodle 2.8
+     */
+    private static function grades_table_column() {
+        return array (
+            'class'   => new external_value(PARAM_RAW, 'class'),
+            'content' => new external_value(PARAM_RAW, 'cell content'),
+            'headers' => new external_value(PARAM_RAW, 'headers')
+        );
+    }
+
+    /**
+     * Describes tget_grades_table return value.
+     *
+     * @return external_single_structure
+     * @since Moodle 2.8
+     */
+    public static function gradereport_user_get_grades_table_returns() {
+        return new external_single_structure(
+            array(
+                'tables' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'courseid' => new external_value(PARAM_INT, 'course id'),
+                            'userid'   => new external_value(PARAM_INT, 'user id'),
+                            'userfullname' => new external_value(PARAM_TEXT, 'user fullname'),
+                            'maxdepth'   => new external_value(PARAM_INT, 'table max depth (needed for printing it)'),
+                            'tabledata' => new external_multiple_structure(
+                                new external_single_structure(
+                                    array(
+                                        'itemname' => new external_single_structure(
+                                            array (
+                                                'class' => new external_value(PARAM_RAW, 'file name'),
+                                                'colspan' => new external_value(PARAM_INT, 'mime type'),
+                                                'content'  => new external_value(PARAM_RAW, ''),
+                                                'celltype'  => new external_value(PARAM_RAW, ''),
+                                                'id'  => new external_value(PARAM_ALPHANUMEXT, '')
+                                            ), 'The item returned data', VALUE_OPTIONAL
+                                        ),
+                                        'leader' => new external_single_structure(
+                                            array (
+                                                'class' => new external_value(PARAM_RAW, 'file name'),
+                                                'rowspan' => new external_value(PARAM_INT, 'mime type')
+                                            ), 'The item returned data', VALUE_OPTIONAL
+                                        ),
+                                        'weight' => new external_single_structure(
+                                            self::grades_table_column(), 'weight column', VALUE_OPTIONAL
+                                        ),
+                                        'grade' => new external_single_structure(
+                                            self::grades_table_column(), 'grade column', VALUE_OPTIONAL
+                                        ),
+                                        'range' => new external_single_structure(
+                                            self::grades_table_column(), 'range column', VALUE_OPTIONAL
+                                        ),
+                                        'percentage' => new external_single_structure(
+                                            self::grades_table_column(), 'percentage column', VALUE_OPTIONAL
+                                        ),
+                                        'lettergrade' => new external_single_structure(
+                                            self::grades_table_column(), 'lettergrade column', VALUE_OPTIONAL
+                                        ),
+                                        'rank' => new external_single_structure(
+                                            self::grades_table_column(), 'rank column', VALUE_OPTIONAL
+                                        ),
+                                        'average' => new external_single_structure(
+                                            self::grades_table_column(), 'average column', VALUE_OPTIONAL
+                                        ),
+                                        'feedback' => new external_single_structure(
+                                            self::grades_table_column(), 'feedback column', VALUE_OPTIONAL
+                                        ),
+                                        'contributiontocoursetotal' => new external_single_structure(
+                                            self::grades_table_column(), 'contributiontocoursetotal column', VALUE_OPTIONAL
+                                        ),
+                                    ), 'table'
+                                )
+                            )
+                        )
+                    )
+                ),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
 }
