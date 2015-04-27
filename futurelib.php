5423b25e2d37b66487c58176d76166e3d3833377
Juan Leyva <juanleyvadelgado@gmail.com>
2015-04-27 13:48:31 +0200

    MOBILE-940 logging: Add future function get_course_and_cm_from_instance

--- futurelib.php
+++ futurelib.php
@@ -53,3 +53,84 @@ if (!function_exists('user_remove_user_device')) {
         return true;
     }
 }
+
+if (!function_exists('get_course_and_cm_from_instance')) {
+    /**
+     * Efficiently retrieves the $course (stdclass) and $cm (cm_info) objects, given
+     * an instance id or record and module name.
+     *
+     * Usage:
+     * list($course, $cm) = get_course_and_cm_from_instance($forum, 'forum');
+     *
+     * Using this method has a performance advantage because it works by loading
+     * modinfo for the course - which will then be cached and it is needed later
+     * in most requests. It also guarantees that the $cm object is a cm_info and
+     * not a stdclass.
+     *
+     * The $course object can be supplied if already known and will speed
+     * up this function - although it is more efficient to use this function to
+     * get the course if you are starting from an instance id.
+     *
+     * By default this obtains information (for example, whether user can access
+     * the activity) for current user, but you can specify a userid if required.
+     *
+     * @param stdclass|int $instanceorid Id of module instance, or database object
+     * @param string $modulename Modulename (required)
+     * @param stdClass|int $courseorid Optional course object if already loaded
+     * @param int $userid Optional userid (default = current)
+     * @return array Array with 2 elements $course and $cm
+     * @throws moodle_exception If the item doesn't exist or is of wrong module name
+     */
+    function get_course_and_cm_from_instance($instanceorid, $modulename, $courseorid = 0, $userid = 0) {
+        global $DB;
+
+        // Get data from parameter.
+        if (is_object($instanceorid)) {
+            $instanceid = $instanceorid->id;
+            if (isset($instanceorid->course)) {
+                $courseid = (int)$instanceorid->course;
+            } else {
+                $courseid = 0;
+            }
+        } else {
+            $instanceid = (int)$instanceorid;
+            $courseid = 0;
+        }
+
+        // Get course from last parameter if supplied.
+        $course = null;
+        if (is_object($courseorid)) {
+            $course = $courseorid;
+        } else if ($courseorid) {
+            $courseid = (int)$courseorid;
+        }
+
+        // Validate module name if supplied.
+        if (!core_component::is_valid_plugin_name('mod', $modulename)) {
+            throw new coding_exception('Invalid modulename parameter');
+        }
+
+        if (!$course) {
+            if ($courseid) {
+                // If course ID is known, get it using normal function.
+                $course = get_course($courseid);
+            } else {
+                // Get course record in a single query based on instance id.
+                $pagetable = '{' . $modulename . '}';
+                $course = $DB->get_record_sql("
+                        SELECT c.*
+                          FROM $pagetable instance
+                          JOIN {course} c ON c.id = instance.course
+                         WHERE instance.id = ?", array($instanceid), MUST_EXIST);
+            }
+        }
+
+        // Get cm from get_fast_modinfo.
+        $modinfo = get_fast_modinfo($course, $userid);
+        $instances = $modinfo->get_instances_of($modulename);
+        if (!array_key_exists($instanceid, $instances)) {
+            throw new moodle_exception('invalidmoduleid', 'error', $instanceid);
+        }
+        return array($course, $instances[$instanceid]);
+    }
+}
