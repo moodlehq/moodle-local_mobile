55b63db7bbd82d903b7c5ded61cfc1921e2b5962
Juan Leyva <juanleyvadelgado@gmail.com>
2015-07-24 11:44:23 +0200

    Bump version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014052817;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014052818;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012120300;            // Requires this Moodle version.
 $plugin->component = 'local_mobile';        // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
