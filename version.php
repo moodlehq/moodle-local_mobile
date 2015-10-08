dc0aaf47e01e314d202274512c4772a0c44e30f2
Juan Leyva <juanleyvadelgado@gmail.com>
2015-10-08 10:14:53 +0200

    Release version for Moodle Mobile 2.3

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015050106;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015050107;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;            // Requires this Moodle version.
 $plugin->component = 'local_mobile';        // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.9.21';
+$plugin->release   = '2.9.23';
