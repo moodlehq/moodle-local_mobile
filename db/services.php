7b2f7570fa221f6cb997bff9ad6e4528c2a0d13a
Juan Leyva <juanleyvadelgado@gmail.com>
2015-10-19 09:10:03 +0200

    Bump version for Moodle Mobile 2.4

--- db/services.php
+++ db/services.php
@@ -353,6 +353,34 @@ $functions = array(
         'capabilities'  => 'mod/choice:choose'
     ),
 
+    'local_mobile_mod_survey_get_questions' => array(
+        'classname'     => 'local_mobile_external',
+        'methodname'    => 'mod_survey_get_questions',
+        'classpath'     => 'local/mobile/externallib.php',
+        'description'   => 'Get the complete list of questions for the survey, including subquestions.',
+        'type'          => 'read',
+        'capabilities'  => 'mod/survey:participate'
+    ),
+
+    'local_mobile_mod_survey_get_surveys_by_courses' => array(
+        'classname'     => 'local_mobile_external',
+        'methodname'    => 'mod_survey_get_surveys_by_courses',
+        'classpath'     => 'local/mobile/externallib.php',
+        'description'   => 'Returns a list of survey instances in a provided set of courses,
+                            if no courses are provided then all the survey instances the user has access to will be returned.',
+        'type'          => 'read',
+        'capabilities'  => ''
+    ),
+
+    'local_mobile_mod_survey_submit_answers' => array(
+        'classname'     => 'local_mobile_external',
+        'methodname'    => 'mod_survey_submit_answers',
+        'classpath'     => 'local/mobile/externallib.php',
+        'description'   => 'Submit the answers for a given survey.',
+        'type'          => 'write',
+        'capabilities'  => 'mod/survey:participate'
+    ),
+
 );
 
 $services = array(
@@ -414,6 +442,9 @@ $services = array(
             'local_mobile_mod_lti_get_ltis_by_courses',
             'local_mobile_core_completion_mark_course_self_completed',
             'local_mobile_mod_choice_delete_choice_responses',
+            'local_mobile_mod_survey_get_questions',
+            'local_mobile_mod_survey_get_surveys_by_courses',
+            'local_mobile_mod_survey_submit_answers',
         ),
         'enabled' => 0,
         'restrictedusers' => 0,
--- externallib.php
+++ externallib.php
@@ -4893,4 +4893,305 @@ class local_mobile_external extends external_api {
         );
     }
 
+
+    /**
+     * Describes the parameters for get_surveys_by_courses.
+     *
+     * @return external_external_function_parameters
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_get_surveys_by_courses_parameters() {
+        return new external_function_parameters (
+            array(
+                'courseids' => new external_multiple_structure(
+                    new external_value(PARAM_INT, 'course id'), 'Array of course ids', VALUE_DEFAULT, array()
+                ),
+            )
+        );
+    }
+
+    /**
+     * Returns a list of surveys in a provided list of courses,
+     * if no list is provided all surveys that the user can view will be returned.
+     *
+     * @param array $courseids the course ids
+     * @return array of surveys details
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_get_surveys_by_courses($courseids = array()) {
+        global $CFG, $USER, $DB;
+
+        $returnedsurveys = array();
+        $warnings = array();
+
+        $params = self::validate_parameters(self::mod_survey_get_surveys_by_courses_parameters(), array('courseids' => $courseids));
+
+        if (empty($params['courseids'])) {
+            $params['courseids'] = array_keys(enrol_get_my_courses());
+        }
+
+        // Ensure there are courseids to loop through.
+        if (!empty($params['courseids'])) {
+
+            list($courses, $warnings) = external_util::validate_courses($params['courseids']);
+
+            // Get the surveys in this course, this function checks users visibility permissions.
+            // We can avoid then additional validate_context calls.
+            $surveys = get_all_instances_in_courses("survey", $courses);
+            foreach ($surveys as $survey) {
+                $context = context_module::instance($survey->coursemodule);
+                // Entry to return.
+                $surveydetails = array();
+                // First, we return information that any user can see in the web interface.
+                $surveydetails['id'] = $survey->id;
+                $surveydetails['coursemodule']      = $survey->coursemodule;
+                $surveydetails['course']            = $survey->course;
+                $surveydetails['name']              = external_format_string($survey->name, $context->id);
+
+                if (has_capability('mod/survey:participate', $context)) {
+                    $trimmedintro = trim($survey->intro);
+                    if (empty($trimmedintro)) {
+                        $tempo = $DB->get_field("survey", "intro", array("id" => $survey->template));
+                        $survey->intro = get_string($tempo, "survey");
+                    }
+
+                    // Format intro.
+                    list($surveydetails['intro'], $surveydetails['introformat']) =
+                        external_format_text($survey->intro, $survey->introformat, $context->id, 'mod_survey', 'intro', null);
+
+                    $surveydetails['template']  = $survey->template;
+                    $surveydetails['days']      = $survey->days;
+                    $surveydetails['questions'] = $survey->questions;
+                    $surveydetails['surveydone'] = survey_already_done($survey->id, $USER->id) ? 1 : 0;
+
+                }
+
+                if (has_capability('moodle/course:manageactivities', $context)) {
+                    $surveydetails['timecreated']   = $survey->timecreated;
+                    $surveydetails['timemodified']  = $survey->timemodified;
+                    $surveydetails['section']       = $survey->section;
+                    $surveydetails['visible']       = $survey->visible;
+                    $surveydetails['groupmode']     = $survey->groupmode;
+                    $surveydetails['groupingid']    = $survey->groupingid;
+                }
+                $returnedsurveys[] = $surveydetails;
+            }
+        }
+        $result = array();
+        $result['surveys'] = $returnedsurveys;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Describes the get_surveys_by_courses return value.
+     *
+     * @return external_single_structure
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_get_surveys_by_courses_returns() {
+        return new external_single_structure(
+            array(
+                'surveys' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'id' => new external_value(PARAM_INT, 'Survey id'),
+                            'coursemodule' => new external_value(PARAM_INT, 'Course module id'),
+                            'course' => new external_value(PARAM_INT, 'Course id'),
+                            'name' => new external_value(PARAM_RAW, 'Survey name'),
+                            'intro' => new external_value(PARAM_RAW, 'The Survey intro', VALUE_OPTIONAL),
+                            'introformat' => new external_format_value('intro', VALUE_OPTIONAL),
+                            'template' => new external_value(PARAM_INT, 'Survey type', VALUE_OPTIONAL),
+                            'days' => new external_value(PARAM_INT, 'Days', VALUE_OPTIONAL),
+                            'questions' => new external_value(PARAM_RAW, 'Question ids', VALUE_OPTIONAL),
+                            'surveydone' => new external_value(PARAM_INT, 'Did I finish the survey?', VALUE_OPTIONAL),
+                            'timecreated' => new external_value(PARAM_INT, 'Time of creation', VALUE_OPTIONAL),
+                            'timemodified' => new external_value(PARAM_INT, 'Time of last modification', VALUE_OPTIONAL),
+                            'section' => new external_value(PARAM_INT, 'Course section id', VALUE_OPTIONAL),
+                            'visible' => new external_value(PARAM_INT, 'Visible', VALUE_OPTIONAL),
+                            'groupmode' => new external_value(PARAM_INT, 'Group mode', VALUE_OPTIONAL),
+                            'groupingid' => new external_value(PARAM_INT, 'Group id', VALUE_OPTIONAL),
+                        ), 'Surveys'
+                    )
+                ),
+                'warnings' => new external_warnings(),
+            )
+        );
+    }
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_get_questions_parameters() {
+        return new external_function_parameters(
+            array(
+                'surveyid' => new external_value(PARAM_INT, 'survey instance id')
+            )
+        );
+    }
+
+    /**
+     * Get the complete list of questions for the survey, including subquestions.
+     *
+     * @param int $surveyid the survey instance id
+     * @return array of warnings and the question list
+     * @since Moodle 3.0
+     * @throws moodle_exception
+     */
+    public static function mod_survey_get_questions($surveyid) {
+        global $DB, $USER;
+
+        $params = self::validate_parameters(self::mod_survey_get_questions_parameters(),
+                                            array(
+                                                'surveyid' => $surveyid
+                                            ));
+        $warnings = array();
+
+        // Request and permission validation.
+        $survey = $DB->get_record('survey', array('id' => $params['surveyid']), '*', MUST_EXIST);
+        list($course, $cm) = get_course_and_cm_from_instance($survey, 'survey');
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/survey:participate', $context);
+
+        $mainquestions = survey_get_questions($survey);
+
+        foreach ($mainquestions as $question) {
+            if ($question->type >= 0) {
+                // Parent is used in subquestions.
+                $question->parent = 0;
+                $questions[] = survey_translate_question($question);
+
+                // Check if the question has subquestions.
+                if ($question->multi) {
+                    $subquestions = survey_get_subquestions($question);
+                    foreach ($subquestions as $sq) {
+                        $sq->parent = $question->id;
+                        $questions[] = survey_translate_question($sq);
+                    }
+                }
+            }
+        }
+
+        $result = array();
+        $result['questions'] = $questions;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_description
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_get_questions_returns() {
+        return new external_single_structure(
+            array(
+                'questions' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'id' => new external_value(PARAM_INT, 'Question id'),
+                            'text' => new external_value(PARAM_RAW, 'Question text'),
+                            'shorttext' => new external_value(PARAM_RAW, 'Question short text'),
+                            'multi' => new external_value(PARAM_RAW, 'Subquestions ids'),
+                            'intro' => new external_value(PARAM_RAW, 'The question intro'),
+                            'type' => new external_value(PARAM_INT, 'Question type'),
+                            'options' => new external_value(PARAM_RAW, 'Question options'),
+                            'parent' => new external_value(PARAM_INT, 'Parent question (for subquestions)'),
+                        ), 'Questions'
+                    )
+                ),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
+
+    /**
+     * Describes the parameters for submit_answers.
+     *
+     * @return external_function_parameters
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_submit_answers_parameters() {
+        return new external_function_parameters(
+            array(
+                'surveyid' => new external_value(PARAM_INT, 'Survey id'),
+                'answers' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'key' => new external_value(PARAM_RAW, 'Answer key'),
+                            'value' => new external_value(PARAM_RAW, 'Answer value')
+                        )
+                    )
+                ),
+            )
+        );
+    }
+
+    /**
+     * Submit the answers for a given survey.
+     *
+     * @param int $surveyid the survey instance id
+     * @param array $answers the survey answers
+     * @return array of warnings and status result
+     * @since Moodle 3.0
+     * @throws moodle_exception
+     */
+    public static function mod_survey_submit_answers($surveyid, $answers) {
+        global $DB, $USER;
+
+        $params = self::validate_parameters(self::mod_survey_submit_answers_parameters(),
+                                            array(
+                                                'surveyid' => $surveyid,
+                                                'answers' => $answers
+                                            ));
+        $warnings = array();
+
+        // Request and permission validation.
+        $survey = $DB->get_record('survey', array('id' => $params['surveyid']), '*', MUST_EXIST);
+        list($course, $cm) = get_course_and_cm_from_instance($survey, 'survey');
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/survey:participate', $context);
+
+        if (survey_already_done($survey->id, $USER->id)) {
+            throw new moodle_exception("alreadysubmitted", "survey");
+        }
+
+        // Build the answers array. Data is cleaned inside the survey_save_answers function.
+        $answers = array();
+        foreach ($params['answers'] as $answer) {
+            $key = $answer['key'];
+            $answers[$key] = $answer['value'];
+        }
+
+        survey_save_answers($survey, $answers, $course, $context);
+
+        $result = array();
+        $result['status'] = true;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_description
+     * @since Moodle 3.0
+     */
+    public static function mod_survey_submit_answers_returns() {
+        return new external_single_structure(
+            array(
+                'status' => new external_value(PARAM_BOOL, 'status: true if success'),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
+
 }
