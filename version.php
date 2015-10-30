58df3f1e95627658f8f5c39c1790d91e8fe30202
Juan Leyva <juanleyvadelgado@gmail.com>
2015-10-30 15:29:47 +0100

    MOBILE-1286 Bump version numbers for Moodle Mobile 2.5

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015050108;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015050109;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;            // Requires this Moodle version.
 $plugin->component = 'local_mobile';        // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.9.24';
+$plugin->release   = '2.9.25';
