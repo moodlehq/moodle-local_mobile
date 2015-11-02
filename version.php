667dce536f0a157d86f728e042eba9acf8bd7e6c
Juan Leyva <juanleyvadelgado@gmail.com>
2015-11-02 10:47:24 +0100

    Remove search and self-enrol functions

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014060217;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014060218;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013051400;            // Requires this Moodle version.
 $plugin->component = 'local_mobile';        // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
