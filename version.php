ff93f860670911a071f64588ca687ccb6584de91
Juan Leyva <juanleyvadelgado@gmail.com>
2016-05-06 10:19:40 +0200

    MOBILE-1571: Bump version number for Moodle Mobile 3.1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014060425;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014060426;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;            // Requires this Moodle version.
 $plugin->component = 'local_mobile';        // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.7.300';
+$plugin->release   = '2.7.310';
